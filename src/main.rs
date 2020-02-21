6225e947cb86b6871b02225c2dae0aa0fbe86b5e
Alex Culliere <nachasic@gmail.com>
2020-02-22 01:24:33 +0200

    WIP porting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,131 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+mod template;
+use template::{ Package };
+
+lazy_static! {
+    static ref LATEX_ENVIRONMENTS: HashMap<&'static str, &'static str> = {
+        let mut map = HashMap::with_capacity(7);
+        map.insert("eqnarray", "eqnarray");
+        map.insert("tikzcd", "tikz-cd");
+        map.insert("tikzpicture", "tikz");
+        map.insert("circuitikz", "circuitikz");
+        map.insert("sequencediagram", "pgf-umlsd");
+        map.insert("prooftree", "bussproofs");
+        map.insert("align", "");
+        return map
+    };
+
+    static ref LATEX_COMMANDS: HashMap<&'static str, &'static str> = {
+        let mut map = HashMap::with_capacity(4);
+        map.insert("\\addplot", "pgfplots");
+        map.insert("\\smartdiagram", "smartdiagram");
+        map.insert("\\DisplayProof", "bussproofs");
+        map.insert("\\tdplotsetmaincoords", "tikz-3dplot");
+        return map
+    };
+
+    static ref LATEX_COMMANDS_INLINE: HashMap<&'static str, &'static str> = {
+        let mut map = HashMap::with_capacity(7);
+        map.insert("\\color", "xcolor");
+        map.insert("\\textcolor", "xcolor");
+        map.insert("\\colorbox", "xcolor");
+        map.insert("\\pagecolor", "xcolor");
+        return map
+    };
+}
+
+#[derive(Debug)]
+pub struct Formula {
+    text: String,
+    has_baseline: bool,
+}
+
+impl From<&str> for Formula {
+    fn from (formula: &str) -> Formula {
+        let mut formula_str = String::from(formula);
+        let mut is_math_mode: bool = true;
+        let mut extra_packages: HashMap<&str, Package> = HashMap::new();
+
+        // Check if there are used certain environments and include corresponding packages
+        for (command, env) in LATEX_ENVIRONMENTS.iter() {
+            let command_str = format!("\\begin{{{}}}", command);
+            let command_asterik_str = format!("\\begin{{{}*}}", command);
+
+            if formula_str.contains(&command_str) || formula_str.contains(&command_asterik_str) {
+                is_math_mode = false;
+                extra_packages.insert(env, Package {
+                    options: vec![],
+                    package_str: env.to_string()
+                });
+            }
+        };
+
+        // Check if there are used certain commands and include corresponding packages
+        for (command, env) in LATEX_COMMANDS.iter() {
+            if formula_str.contains(command) {
+                is_math_mode = false;
+                extra_packages.insert(env, Package {
+                    options: vec![],
+                    package_str: env.to_string()
+                });
+            }
+        };
+
+        // Same as above but for inline commands inside math mode
+        for (command, env) in LATEX_COMMANDS_INLINE.iter() {
+            if formula_str.contains(command) {
+                is_math_mode = false;
+                extra_packages.insert(env, Package {
+                    options: vec![],
+                    package_str: env.to_string()
+                });
+            }
+        };
+
+        // Custom rules
+        if formula_str.contains("\\xymatrix") || formula_str.contains("\\begin{xy}") {
+            extra_packages.insert("xy", Package {
+                options: vec!["all"],
+                package_str: "xy".to_string(),
+            });
+        };
+
+        // Other setup
+        let is_inline: bool = {
+            let inline_matches: Vec<(usize, &str)> = formula.match_indices("\\inline").collect();
+            
+            if inline_matches.len() == 0 {
+                false
+            } else {
+                let (index, _) = inline_matches[0];
+                index == 1
+            }
+        };
+
+        if is_inline {
+            // Replace "\inline" with "\textstyle "
+            formula_str = format!("\\textstyle {}", &formula_str[7..]);
+        };
+
+		// $tpl = $isMathMode ? 'displayformula' : 'common';
+
+		// ob_start();
+		// include $this->dir . $tpl . '.php';
+		// $documentContent = ob_get_clean();
+
+		// ob_start();
+		// include $this->dir . 'document.php';
+		// $text = ob_get_clean();
+
+		// return new Formula($text, $isMathMode);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut st = "\\inline|lol look at this \\begin{xy} shit dude".to_string();
+    st = format!("\\textstyle {}", &st[7..]);
+    println!("{}", st);
 }
---
+++ src/template/mod.rs
@@ -0,0 +1,29 @@
+pub struct Package<'a> {
+    options: Vec<&'a str>,
+    package_str: String,
+}
+
+impl <'a> Package<'a> {
+    pub fn get_code(&self) -> String {
+        format!("\\usepackage{}{{{}}}", &self.get_options(), &self.package_str)
+    }
+
+    fn get_options(&self) -> String {
+        match self.options.len() {
+            0 => "".to_string(),
+            _ => format!("[{}]", &self.options.join(","))
+        }
+    }
+}
+
+#[test]
+fn basic_test () {
+    let pack = Package {
+        package_str: "Test package string".to_string(),
+        options: vec!["foo", "bar"]
+    };
+    assert_eq!(&pack.get_code(),
+        "\\usepackage[foo,bar]{Test package string}"
+    );
+}
+
